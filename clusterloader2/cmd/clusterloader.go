b17aeb229adc273d16a96e9582ca2e5aa38436ad
xuehaishan <xuehaishan@kingsoft.com>
2019-01-13 10:25:14 +0800

    test
    Signed-off-by: xuehaishan <xuehaishan@kingsoft.com>

--- clusterloader2/cmd/clusterloader.go
+++ clusterloader2/cmd/clusterloader.go
@@ -19,6 +19,10 @@ package main
 import (
 	"fmt"
 	"github.com/golang/glog"
+	ginkgoconfig "github.com/onsi/ginkgo/config"
+	ginkgoreporters "github.com/onsi/ginkgo/reporters"
+	ginkgotypes "github.com/onsi/ginkgo/types"
+
 	"k8s.io/perf-tests/clusterloader2/pkg/config"
 	"k8s.io/perf-tests/clusterloader2/pkg/errors"
 	"k8s.io/perf-tests/clusterloader2/pkg/flags"
