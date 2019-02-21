f4bcfddf45e78386e4852d735e681d8293d730f3
seamount-knight <15010828953@163.com>
2019-02-21 21:09:27 +0800

    test
    Signed-off-by: seamount-knight <15010828953@163.com>

--- restore/main.go
+++ restore/main.go
@@ -28,8 +28,17 @@ func main() {
 }
 
 func check_master_node() {
-	cmdStr := "kubectl  get node 10.21.128.13 | grep 10.21.128.13 | awk {'print $2'}"
-	cmd := exec.Command(cmdStr)
+
+	in := bytes.NewBuffer(nil)
+	cmd := exec.Command("sh")
+	cmd.Stdin = in
+	go func() {
+		in.WriteString("kubectl  get node 10.21.128.13 | grep 10.21.128.13 | awk {'print $2'}")
+		in.WriteString("exit\n")
+	}()
+
+	// cmdStr := "kubectl  get node 10.21.128.13 | grep 10.21.128.13 | awk {'print $2'}"
+	// cmd := exec.Command(cmdStr)
 
 	var out bytes.Buffer
 	cmd.Stdout = &out //输出
