761548532b2331bf41d6e14556238f32ae47e7cd
seamount-knight <15010828953@163.com>
2019-02-21 21:32:14 +0800

    test
    Signed-off-by: seamount-knight <15010828953@163.com>

--- restore/main.go
+++ restore/main.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"log"
-	"net/http"
 	"os/exec"
 	"strings"
 	"time"
@@ -25,10 +24,12 @@ func initFlags() {
 }
 
 func main() {
-	check_master_node()
+	cmdStr := "curl -v https://10.21.128.13:6443/healthz --insecure | grep ok\n"
+	checkMasterSvc(cmdStr)
+	// check_node_ready()
 }
 
-func check_master_node() {
+func check_node_ready() {
 
 	// go func() {
 	// 	in.WriteString("kubectl  get node 10.21.128.13 | grep 10.21.128.13 | awk {'print $2'}\n")
@@ -75,19 +76,61 @@ func check_master_node() {
 
 }
 
-func checkServer(url string) bool {
-	for {
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Println(err)
-			time.Sleep(500 * time.Millisecond)
-		}
-		if resp.StatusCode == http.StatusOK {
-			fmt.Println("ok")
-			return true
-		}
-		fmt.Println(resp.StatusCode)
-		time.Sleep(500 * time.Millisecond)
-	}
+func checkMasterSvc(cmdstr string) {
+	flag1 := false
+	flag2 := false
 
+	var now time.Time
+	var dura time.Duration
+
+	for !flag1 {
+		in := bytes.NewBuffer(nil)
+		cmd := exec.Command("sh")
+		var out bytes.Buffer
+		cmd.Stdout = &out //输出
+
+		cmd.Stdin = in
+		in.WriteString(cmdstr)
+
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		status := strings.TrimSpace(out.String())
+		fmt.Printf("node status: %q\n", status)
+		if status == "ok" && !flag1 {
+			time.Sleep(500 * time.Millisecond)
+			continue
+		}
+		fmt.Println(status)
+		if status == "" && !flag2 {
+			flag2 = true
+			now = time.Now()
+		}
+		if status == "ok" && !flag2 {
+			time.Sleep(500 * time.Millisecond)
+			dura = time.Since(now)
+			fmt.Println("dura", dura)
+			flag1 = true
+			return
+		}
+	}
 }
+
+// func checkMasterServer(host string) bool {
+// 	for {
+// 		resp, err := http.Get(url)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			time.Sleep(500 * time.Millisecond)
+// 		}
+// 		if resp.StatusCode == http.StatusOK {
+// 			fmt.Println("ok")
+// 			return true
+// 		}
+// 		fmt.Println(resp.StatusCode)
+// 		time.Sleep(500 * time.Millisecond)
+// 	}
+
+// }
