47c06dd4c32f2b2f5731512c05bdf267f0c94553
seamount-knight <15010828953@163.com>
2019-02-22 13:50:37 +0800

    test
    Signed-off-by: seamount-knight <15010828953@163.com>

--- restore/main.go
+++ restore/main.go
@@ -24,23 +24,18 @@ func initFlags() {
 }
 
 func main() {
-	cmdStr := "curl -v https://10.21.128.13:6443/healthz --insecure | grep ok\n"
-	checkMasterSvc(cmdStr)
-	// check_node_ready()
+	// cmdStr := "curl -v https://10.21.128.13:6443/healthz --insecure | grep ok\n"
+	// checkMasterSvc(cmdStr)
+	check_node_ready()
 }
 
 func check_node_ready() {
 
-	// go func() {
-	// 	in.WriteString("kubectl  get node 10.21.128.13 | grep 10.21.128.13 | awk {'print $2'}\n")
-	// }()
-	flag1 := false
-	flag2 := false
-
 	var now time.Time
 	var dura time.Duration
+	now = time.Now()
 
-	for !flag1 {
+	for {
 		in := bytes.NewBuffer(nil)
 		cmd := exec.Command("sh")
 		var out bytes.Buffer
@@ -56,24 +51,17 @@ func check_node_ready() {
 
 		status := strings.TrimSpace(out.String())
 		fmt.Printf("node status: %q\n", status)
-		if status == "Ready" && !flag1 {
-			time.Sleep(500 * time.Millisecond)
-			continue
-		}
-		fmt.Println(status)
-		if status == "NotReady" && !flag2 {
-			flag2 = true
-			now = time.Now()
-		}
-		if status == "Ready" && !flag2 {
-			time.Sleep(500 * time.Millisecond)
+
+		if status == "Ready" {
 			dura = time.Since(now)
-			fmt.Println("dura", dura)
-			flag1 = true
 			return
 		}
-	}
 
+		if status == "NotReady" {
+			time.Sleep(1 * time.Second)
+		}
+	}
+	fmt.Println("duration: ", dura)
 }
 
 func checkMasterSvc(cmdstr string) {
