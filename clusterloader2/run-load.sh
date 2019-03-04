5228c732d299ef223bf4e1c879168a6d85f780cd
seamounts <15010828953@163.com>
2019-03-04 16:55:06 +0800

    test

--- clusterloader2/run-load.sh
+++ clusterloader2/run-load.sh
@@ -1,3 +1,3 @@
-export MASTER_NAME=10.21.128.6
+export MASTER_NAME=10.21.128.13
 export MasterSSHPass=oue1W2Ks3
-/bin/bash -c "go run cmd/clusterloader.go --kubeconfig=/root/.kube/config --testconfig=./testing/load/config.yaml --provider=kubemark --masterip=10.21.128.6  --report-dir=./report-load --logtostderr=true"
+/bin/bash -c "go run cmd/clusterloader.go --kubeconfig=/root/.kube/config --testconfig=./testing/load/config.yaml --provider=kubemark --masterip=10.21.128.13  --report-dir=./report-load --logtostderr=true"
