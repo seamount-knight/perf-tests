2abaf7e2995b8e39d36ab17ff64957bfaaa15b03
root <root@vm10-21-64-21.ksc.com>
2019-03-13 10:41:20 +0800

    test 500

--- clusterloader2/run-load.sh
+++ clusterloader2/run-load.sh
@@ -1,3 +1,3 @@
-export MASTER_NAME=10.21.128.13
-export MasterSSHPass=oue1W2Ks3
-/bin/bash -c "go run cmd/clusterloader.go --kubeconfig=/root/.kube/config --testconfig=./testing/load/config.yaml --provider=kubemark --masterip=10.21.128.13  --report-dir=./report-load --logtostderr=true"
+export MASTER_NAME=10.21.64.18
+export MasterSSHPass=tawFOFKs3
+/bin/bash -c "go run cmd/clusterloader.go --kubeconfig=/root/.kube/config --testconfig=./testing/load/config.yaml --provider=kubemark --masterip=10.21.64.18  --report-dir=./report-load --logtostderr=true"
