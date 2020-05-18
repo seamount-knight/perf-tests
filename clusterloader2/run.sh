2eeecda7a5d99fc2e210a068ef04fa6b0ef96a9b
XUEHAISHAN <xuehaishan@kingsoft.com>
2020-05-18 18:16:17 +0800

    test big cluster

--- clusterloader2/run.sh
+++ clusterloader2/run.sh
@@ -1,1 +1,4 @@
-export MASTER_NODES=
+export MASTER_NODES=10.0.6.222,10.0.6.51,10.0.6.84
+export LOCAL_SSH_KEY=/Users/xuehaishan/Workspace/ssh-key/new
+./clusterloader --logtostderr -v 9 --kubeconfig=./config --masterip=10.0.6.222,10.0.6.51,10.0.6.84  --provider=local --report-dir=./report --testconfig=./testing/load/config.yaml
+ 
